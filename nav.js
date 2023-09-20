bb9c8a36313e0571014486c89ccc662c070f7ebd
Amoako Domson Wahab <wamoako_domson@st.ug.edu.gh>
2023-09-20 15:46:39 +0000

    update

---
+++ nav.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".ham-navbar");
+  const icon = document.querySelector(".ham-icon");
+  menu.classList.toggle("active");
+  icon.classList.toggle("active");
+}
